01c8d70e8e783e034f144fa6b90e0c221f6e1e92
mscrawfordcodes <emily.crawford@tdsb.on.ca>
2024-07-16 16:23:19 -0400

    updating examples

--- 19_pygame_example.py
+++ 19_pygame_example.py
@@ -10,7 +10,7 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-while running:
+while running: 
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
